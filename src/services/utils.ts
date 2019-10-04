e335000052e316c49a961d70d36b1f8edd13b218
augustin <augustin.chini@hotmail.fr>
2019-10-03 21:32:34 -0400

    fix debounce default waiting time

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -4,7 +4,7 @@ export class Utils {
       * @see https://davidwalsh.name/javascript-debounce-function
       * @todo it's a reusable function should we put it in utils js file?
       */
-    public static debounce(func: () => void, wait = 50) {
+    public static debounce(func: () => void, wait = 10) {
         let h: number;
         return () => {
             clearTimeout(h);
