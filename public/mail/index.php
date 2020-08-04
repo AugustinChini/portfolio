b232d2161633afa08faf36eb7c51f872ea890134
Augustin CHINI <augustin.chini@nist.gov>
2020-08-04 12:29:11 -0400

    last version of the mailer

--- public/mail/index.php
+++ public/mail/index.php
@@ -12,7 +12,12 @@ ini_set('display_startup_errors', TRUE);
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
-require '../vendor/autoload.php';
+require './vendor/autoload.php';
+
+// get all the message params
+$subject = htmlspecialchars($_POST["subject"]);
+$body = htmlspecialchars($_POST["body"]);
+$from = htmlspecialchars($_POST["from"]);
 
 //Create a new PHPMailer instance
 $mail = new PHPMailer;
@@ -42,29 +47,25 @@ $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 $mail->SMTPAuth = true;
 
 //Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = 'XXX@gmail.com';
+$mail->Username = 'xxx.us@gmail.com';
 
 //Password to use for SMTP authentication
-$mail->Password = 'XXX';
+$mail->Password = 'xxx';
 
 //Set who the message is to be sent from
-$mail->setFrom('XXX@gmail.com', 'First Last');
-
-//Set an alternative reply-to address
-$mail->addReplyTo('XXX@gmail.com', 'First Last');
+$mail->setFrom('xxx.us@gmail.com', 'Portfolio Server');
 
 //Set who the message is to be sent to
-$mail->addAddress('XXX@hotmail.fr', 'Augustin CHINI');
+$mail->addAddress('xxx@hotmail.fr', 'Augustin CHINI');
 
 //Set the subject line
-$mail->Subject = 'PHPMailer GMail SMTP test';
+$mail->Subject = $subject;
 
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+// Set the body
+$mail->msgHTML('<h3>-------FROM: ' . $from . ' -------</h3><br/><p>' . $body . '</p>');
 
 //Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
+$mail->AltBody = '';
 
 //Attach an image file
 //$mail->addAttachment('images/phpmailer_mini.png');
@@ -74,28 +75,4 @@ if (!$mail->send()) {
     echo 'Mailer Error: '. $mail->ErrorInfo;
 } else {
     echo 'Message sent!';
-    //Section 2: IMAP
-    //Uncomment these to save your message in the 'Sent Mail' folder.
-    #if (save_mail($mail)) {
-    #    echo "Message saved!";
-    #}
-}
-
-//Section 2: IMAP
-//IMAP commands requires the PHP IMAP Extension, found at: https://php.net/manual/en/imap.setup.php
-//Function to call which uses the PHP imap_*() functions to save messages: https://php.net/manual/en/book.imap.php
-//You can use imap_getmailboxes($imapStream, '/imap/ssl', '*' ) to get a list of available folders or labels, this can
-//be useful if you are trying to get this working on a non-Gmail IMAP server.
-function save_mail($mail)
-{
-    //You can change 'Sent Mail' to any other folder or tag
-    $path = '{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail';
-
-    //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
-    $imapStream = imap_open($path, $mail->Username, $mail->Password);
-
-    $result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
-    imap_close($imapStream);
-
-    return $result;
 }
